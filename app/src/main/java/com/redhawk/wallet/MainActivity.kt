0002583a0b607c8dca3a05d3af44d283fbb72540
harshvk <harshvk824@gmail.com>
2026-02-11 22:08:00 -0500

    implement login to register navigation

--- app/src/main/java/com/redhawk/wallet/MainActivity.kt
+++ app/src/main/java/com/redhawk/wallet/MainActivity.kt
@@ -3,15 +3,16 @@ package com.redhawk.wallet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.redhawk.wallet.ui.screens.LoginScreen
+import com.redhawk.wallet.ui.screens.AppNav
 import com.redhawk.wallet.ui.theme.RedHawkWalletTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             RedHawkWalletTheme {
-                LoginScreen()
+                AppNav()
             }
         }
     }
