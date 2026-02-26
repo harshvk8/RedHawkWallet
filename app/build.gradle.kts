16fe790b9e6e5d5bb35cd13fd48617d2eeeb707e
harshvk <harshvk824@gmail.com>
2026-02-26 09:23:48 -0500

    Fixing gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application)
-//<<<<<<< feature-ui
-    alias(libs.plugins.kotlin.android)
-=======
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-//>>>>>>> dev
 }
-
 android {
     namespace = "com.redhawk.wallet"
-
-    compileSdk {
-        version = release(36)
-    }
     compileSdk = 34
 
     defaultConfig {
@@ -43,33 +34,26 @@ android {
 }
 
 dependencies {
-//<<<<<<< feature-ui
-
-=======
-    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-//>>>>>>> dev
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
     implementation(platform(libs.androidx.compose.bom))
-
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("com.google.zxing:core:3.5.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("com.google.zxing:core:3.5.3")
+
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+
     implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
 
-
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
