1141bb093977f98e29f064129148d215dc30ee47
Danilo <shotad1@montclair.edu>
2026-03-24 11:23:59 -0400

    Put User Profile.kt in, it was missing

---
+++ app/src/main/java/com/redhawk/wallet/data/models/UserProfile.kt
@@ -0,0 +1,8 @@
+package com.redhawk.wallet.data.models
+
+data class UserProfile(
+    val uid: String = "",
+    val name: String = "",
+    val studentId: String = "",
+    val email: String = ""
+)
