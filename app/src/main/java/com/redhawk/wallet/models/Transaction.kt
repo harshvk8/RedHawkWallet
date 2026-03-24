bc88f27e18c8f6c1c65456a2b3551ce0823aa4cc
Danilo <shotad1@montclair.edu>
2026-03-24 11:26:26 -0400

    Deleted this, it was a duplicate

--- app/src/main/java/com/redhawk/wallet/models/Transaction.kt
+++
@@ -1,8 +0,0 @@
-package com.redhawk.wallet.data.models
-
-data class Transaction(
-    val transactionId: String = "",
-    val walletId: String = "",
-    val amount: Double = 0.0,
-    val type: String = ""
-)
