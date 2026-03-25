cee73eeb60081a0b86af989f1e772e90dca855e7
harshvk <harshvk824@gmail.com>
2026-03-24 20:38:46 -0400

    adding username in UserProfile.kt

--- app/src/main/java/com/redhawk/wallet/data/models/UserProfile.kt
+++ app/src/main/java/com/redhawk/wallet/data/models/UserProfile.kt
@@ -3,6 +3,6 @@ package com.redhawk.wallet.data.models
 data class UserProfile(
     val uid: String = "",
     val name: String = "",
-    val studentId: String = "",
+    val studentId: String = "",  //added username
     val email: String = ""
 )
