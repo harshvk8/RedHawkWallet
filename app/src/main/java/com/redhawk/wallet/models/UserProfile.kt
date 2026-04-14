62710584ba488a25cbfe42fb91424ca115224f0e
rohaifa <yassinr1@montclair.edu>
2026-04-14 19:55:10 -0400

    cimmiting to fix pull AND MERGE CONFLICTS

---
+++ app/src/main/java/com/redhawk/wallet/models/UserProfile.kt
@@ -0,0 +1,8 @@
+package com.redhawk.wallet.data.models
+
+data class UserProfile(
+    val uid: String = "",
+    val fullName: String = "",
+    val email: String = "",
+    val universityId: String = ""
+)
