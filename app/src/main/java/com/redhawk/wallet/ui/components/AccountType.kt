436ffd99ce410e5541edbdb26b108fdafff8e5f8
harshvk <harshvk824@gmail.com>
2026-04-23 01:33:13 -0400

    improved dashboard account selection and transaction UI

--- app/src/main/java/com/redhawk/wallet/ui/components/AccountType.kt
+++ app/src/main/java/com/redhawk/wallet/ui/components/AccountType.kt
@@ -3,7 +3,7 @@ package com.redhawk.wallet.data.models
 enum class AccountType(
     val displayName: String
 ) {
-    RED_HAWK_DOLLARS("Red Hawk Dollars / Debit"),
+    RED_HAWK_DOLLARS("Red Hawk Dollars"),
     FLEX("Flex"),
     BONUS("Bonus"),
     MEAL_SWIPES("Meal Swipes")
